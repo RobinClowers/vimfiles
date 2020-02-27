916fa19fbb1dcb9fee824ea8ad5e79a3f5a4aeb0
Robin Clowers <robin.clowers@ext.airbnb.com>
2020-02-27 09:44:48 -0800

    Add msgpack to setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ if [[ -d "$HOME/.vim/bundle/neobundle.vim" ]]; then
   git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
 
+# required for deoplete.nvim
+pip3 install -U msgpack
+
 vim +NeoBundleInstall +qall
 
 if [[ ! -h "$HOME/.vimrc" ]]; then
