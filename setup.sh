18fc3c9c857b837607e03cb3effa688aa652f4b5
Robin Clowers <robin.clowers@gmail.com>
2021-04-14 18:44:19 -0700

    Fix setup a bit more

--- setup.sh
+++ setup.sh
@@ -2,14 +2,12 @@
 
 set -eo pipefail
 
-if [[ -d "$HOME/.vim/bundle/neobundle.vim" ]]; then
+if [[ ! -d "$HOME/.vim/bundle/neobundle.vim" ]]; then
   git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
 
 # required for deoplete.nvim
 pip3 install -U msgpack
-
-vim +NeoBundleInstall +qall
 
 if [[ ! -h "$HOME/.vimrc" ]]; then
   ln -s ~/.vim/vimrc ~/.vimrc
@@ -17,3 +15,6 @@ fi
 if [[ ! -h "$HOME/.gvimrc" ]]; then
   ln -s ~/.vim/gvimrc ~/.gvimrc
 fi
+
+vim +NeoBundleInstall +qall
+vim +UpdateRemotePlugins +qall
